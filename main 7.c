eb8f334862e789e7ba825ea60a534f7e0b6e4e2e
Emily D. Delgado Gonzalez <emilydelgado010@gmail.com>
2026-01-06 20:07:10 -0500

    Add files via upload

---
+++ main 7.c
@@ -0,0 +1,42 @@
+/* Example StructuresDirect in CodeBlocks
+ * structures as function arguments and return values
+ * Direct instance using names
+ */
+
+#include <stdio.h>
+#include <time.h>
+
+struct cplx {
+    double real;   /* real part */
+    double imag;   /* imaginary part */
+};
+
+struct cplx add(struct cplx a, struct cplx b);   /* function prototype */
+
+int main(void)
+{
+    struct cplx x, y, z;
+    time_t rawtime = time(NULL);
+
+    x.real = 2.5;
+    x.imag = 5.0;
+    y.real = 3.2;
+    y.imag = -1.7;
+
+    z = add(x, y);
+
+    printf("Direct instance z = %4.2f + %4.2fi\n", z.real, z.imag);
+    printf("Today is %s", ctime(&rawtime));
+
+    return 0;
+}
+
+struct cplx add(struct cplx a, struct cplx b)
+{
+    struct cplx c = a;   /* can initialize an auto struct variable */
+
+    c.real += b.real;
+    c.imag += b.imag;
+
+    return c;            /* can return a struct value */
+}
